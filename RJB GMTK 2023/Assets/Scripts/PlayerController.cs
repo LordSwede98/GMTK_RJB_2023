872210f1a33c0bbb7ffd1d34ad8716e440e56648
RJA4 <47540609+RJA4@users.noreply.github.com>
2023-07-08 13:38:05 +0100

    Player Rotation Fixed
    Move to all my stuff to Player Test scene
    Left Main untouched as advised by Ben. Feel free to delete everything in there.

--- RJB GMTK 2023/Assets/Scripts/PlayerController.cs
+++ RJB GMTK 2023/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float playerMoveSpeed = 0.01f;
     public Rigidbody rb;
-
     Vector3 Movement;
-    Vector3 Rotation;
 
     // Update is called once per frame
     void Update()
@@ -19,16 +17,13 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //Movement
         rb.MovePosition(rb.position + Movement * playerMoveSpeed * Time.fixedDeltaTime);
 
+        //Rotation - Please don't ask me how this works.
         Vector3 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 Direction = MousePosition - transform.position;
         float angle = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg;
         rb.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        
-        
-        
-        //Vector3 euler = transform.rotation.eulerAngles;
-        //rb.MoveRotation(Quaternion.Euler(new Vector3(Input.mousePosition.x, -90, 90)));
     }
 }

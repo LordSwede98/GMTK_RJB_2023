22d067e73fdd25fb8527196c3f545cd9bf783cb4
BenN2798 <ben.nash.v4@gmail.com>
2023-07-08 13:00:33 +0100

    UI, Game Controller and TMP
     - Added Timer and Score UI
     - Added GameController class to keep control of everything
     - Imported TMP Essentials

---
+++ RJB GMTK 2023/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] TimerAndScoreController _timeAndScoreControllerPrefab;
+    
+    public static GameController Instance { get; private set; }
+
+    public TimerAndScoreController TimerAndScoreController { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        TimerAndScoreController = Instantiate(_timeAndScoreControllerPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        
+    }
+
+    public void EndGame(int finalScore)
+    {
+
+    }
+}
--- RJB GMTK 2023/Assets/Scripts/TimerAndScoreController.cs
+++ RJB GMTK 2023/Assets/Scripts/TimerAndScoreController.cs
@@ -1,18 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TimerAndScoreController : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI _scoreText;
+
+    [SerializeField] TextMeshProUGUI _timerText;
+    
+    int _score;
+
+    float _timeRemaining;
+
+    bool _timerRunning = false;
+
+    GameController _controller;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _score = 0;
+        _timeRemaining = 30;
+        _controller = GameController.Instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_timerRunning)
+        {
+            if (_timeRemaining > 0)
+            {
+                _timeRemaining -= Time.deltaTime;
+                DisplayTime(_timeRemaining);
+                IncreaseScore(1);
+            }
+            else
+            {
+                _timeRemaining = 0;
+                _timerRunning = false;
+                Debug.Log("TimerFinished");
+                _controller.EndGame(_score);
+            }
+        }
+    }
+
+    public void StartTimer()
+    {
+        _timerRunning = true;
+    }
+
+    void DisplayTime(float currentTimeValue)
+    {
+        if (currentTimeValue < 0)
+            currentTimeValue = 0;
+
+        float minutes = Mathf.FloorToInt(currentTimeValue / 60);
+        float seconds = Mathf.FloorToInt(currentTimeValue % 60);
+
+        _timerText.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+    }
+
+    public void IncreaseScore(int scoreValue)
+    {
+        _score += scoreValue;
+        _scoreText.text = _score.ToString();
     }
 }
